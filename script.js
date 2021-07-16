f1a5c96b188fe4648c9b9caa2fdcba9f135dab6a
Nikong22 <76768475+Nikong22@users.noreply.github.com>
2021-07-16 20:07:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var primera=40;
+var segunda= 16;
+var sumatotal= primera+segunda;
+console.log("La suma entre a y b es:"+ (primera+segunda));
+document.write("<h1>El restulado de la suma de las dos variables es:</h1>"+(primera+segunda));
+
+
+
+
+
+
+
+
+
+
+
+
